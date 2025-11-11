13e58c3b7f855bc6694f61482637be131e8c325c
Paul Ruiz <ruizpint@qut.edu.au>
2025-11-11 18:06:25 +1000

    Finish merge

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, send_from_directory
 
-app = Flask(__name__, template_folder="reports")
+app = Flask(__name__, template_folder="docs")
 app.secret_key = "QUTKey"
 
 @app.route("/", methods=["GET", "POST"])
@@ -10,21 +10,21 @@ def home():
 # ---- REPORT ROUTES ----
 @app.route("/literature")
 def literature():
-    with open("reports/literature.html", "r", encoding="utf-8") as f:
+    with open("docs/literature.html", "r", encoding="utf-8") as f:
         report_content = f.read()
-    return render_template("web.html", content=report_content)
+    return render_template("index.html", content=report_content)
 
 @app.route("/database")
 def database():
-    with open("reports/database.html", "r", encoding="utf-8") as f:
+    with open("docs/database.html", "r", encoding="utf-8") as f:
         report_content = f.read()
-    return render_template("web.html", content=report_content)
+    return render_template("index.html", content=report_content)
 
 @app.route("/report")
 def report():
-    with open("reports/report.html", "r", encoding="utf-8") as f:
+    with open("docs/report.html", "r", encoding="utf-8") as f:
         report_content = f.read()
-    return render_template("web.html", content=report_content)
+    return render_template("index.html", content=report_content)
 
 @app.route("/style/<path:filename>")
 def serve_style(filename):

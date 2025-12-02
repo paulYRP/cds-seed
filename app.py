c3078cd1a14e78ccbd5aa413598c0f55ba575d5b
Paul Ruiz <ruizpint@qut.edu.au>
2025-12-02 17:02:31 +1000

    Approach

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 from flask import Flask, render_template, redirect, url_for, send_from_directory
+import os
 
 app = Flask(__name__, template_folder="docs")
 app.secret_key = "QUTKey"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return redirect(url_for("methods"))
+    return redirect(url_for("synthetic"))
 
 # ---- REPORT ROUTES ----
-@app.route("/methods")
-def methods():
-    with open("docs/methods.html", "r", encoding="utf-8") as f:
+@app.route("/synthetic")
+def synthetic():
+    with open("docs/synthetic.html", "r", encoding="utf-8") as f:
         report_content = f.read()
     return render_template("index.html", content=report_content)
 
-@app.route("/database")
-def database():
-    with open("docs/database.html", "r", encoding="utf-8") as f:
+@app.route("/real")
+def real():
+    with open("docs/real.html", "r", encoding="utf-8") as f:
         report_content = f.read()
     return render_template("index.html", content=report_content)
 
-@app.route("/report")
-def report():
-    with open("docs/report.html", "r", encoding="utf-8") as f:
+@app.route("/summary")
+def summary():
+    with open("docs/summary.html", "r", encoding="utf-8") as f:
+        report_content = f.read()
+    return render_template("index.html", content=report_content)
+
+@app.route("/reference")
+def reference():
+    with open("docs/data.html", "r", encoding="utf-8") as f:
         report_content = f.read()
     return render_template("index.html", content=report_content)
 
